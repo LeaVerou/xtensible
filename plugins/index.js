753fadde43330a51a3ae581ce2d696e7fc52da81
Lea Verou <lea@verou.me>
2026-04-06 12:36:43 -0400

    Common plugin index

---
+++ plugins/index.js
@@ -0,0 +1,4 @@
+export { default as api } from "./api.js";
+export { default as hooksCommon } from "./hooks-common.js";
+export { default as $hook } from "./$hook.js";
+export { default as $super } from "./$super.js";

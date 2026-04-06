2f66afbee47bacd0a3cb486a8d67343cc1fe4c12
Lea Verou <lea@verou.me>
2026-04-06 10:13:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# (E)xtensible
+
+Pluggable classes
---
+++ src/extensible.js
@@ -0,0 +1,6 @@
+export { default as default } from "./base.js";
+export * from "./plugins.js";
+export { default as symbols } from "get-symbols";
+export { default as Hooks } from "./hooks.js";
+export * from "./util/own.js";
+export * from "./util/super.js";
